136ab5131fd850ec205661b8e93799106c1f46b6
Mikko Järvi <mikko.jarvi@helsinki.fi>
2017-03-02 23:44:27 +0200

    data preparations

---
+++ data/create_noshow.R
@@ -0,0 +1,6 @@
+#Mikko Järvi 2.3.2017. This is a R script file for IODS final exercise.
+
+library(dplyr)
+library(stringr)
+
+ns <- read.csv("~/IODS-final/data/No-show-Issue-Comma-300k.csv", stringsAsFactors = FALSE)
