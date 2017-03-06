9306526727cc28c57f1cd15f70096b7cac2a6250
Mikko Järvi <mikko.jarvi@helsinki.fi>
2017-03-06 22:22:07 +0200

    making TOC

--- data/create_noshow.R
+++ data/create_noshow.R
@@ -3,4 +3,8 @@
 library(dplyr)
 library(stringr)
 
+#downloading the data from kaggle.com
+#https://www.kaggle.com/joniarroba/noshowappointments/downloads/medical-appointment-no-shows.zip
+
 ns <- read.csv("~/IODS-final/data/No-show-Issue-Comma-300k.csv", stringsAsFactors = FALSE)
+str(ns)
